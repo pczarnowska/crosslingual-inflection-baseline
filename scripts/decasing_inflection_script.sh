cf00b1ba4206615a5ed85be13d413ebc606b54df
Your Name <you@example.com>
2020-05-29 15:56:56 +0100

    fixes in scripts

--- scripts/decasing_inflection_script.sh
+++ scripts/decasing_inflection_script.sh
@@ -3,7 +3,7 @@ arch=$1
 
 root=/homes/pjc211/project_m2
 
-python src2/train.py \
+python src/train.py \
     --dataset sigmorphon19task1 \
     --train $root/no_lem_nom_inflector_train_adjs_anim_full.txt  \
     --dev $root/no_lem_nom_inflector_test_adjs_anim_full.txt \
--- scripts/mix_lex_reinflection.sh
+++ scripts/mix_lex_reinflection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 arch=$1
-pair=$2
+lang=$2
 datprefix=$3
 outdir=$4
 root=$5
@@ -9,11 +9,11 @@ mname=$6
 if [[ -d $outdir ]]; then
     echo "$outdir exists"
 else
-    python $root/src2/train.py \
+    python $root/src/train.py \
         --dataset sigmorphon19task1 \
         --train $datprefix"_train.txt"  \
         --dev $datprefix"_dev.txt" \
-        --model $outdir/$pair/$arch"_"$mname --seed 0 \
+        --model $outdir/$lang/$arch"_"$mname --seed 0 \
         --embed_dim 200 --src_hs 400 --trg_hs 400 --dropout 0.4 \
         --src_layer 2 --trg_layer 1 --max_norm 5 \
         --arch $arch --estop 1e-8 --epochs 50 --bs 20 --mono --shuffle --patience 10
